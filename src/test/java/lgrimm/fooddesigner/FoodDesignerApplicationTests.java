b8b5480c9e5848e39d214b97e90cf8bd12b4f9ba
lgrimm1 <laszlo.grimm1@gmail.com>
2025-03-03 18:12:03 +0100

    Application context tests for list gateway and list* services.

--- src/test/java/lgrimm/fooddesigner/FoodDesignerApplicationTests.java
+++ src/test/java/lgrimm/fooddesigner/FoodDesignerApplicationTests.java
@@ -1,9 +1,12 @@
 package lgrimm.fooddesigner;
 
 import lgrimm.fooddesigner.gateway.listservices.*;
+import lgrimm.fooddesigner.ingredient.query.listingredients.*;
+import lgrimm.fooddesigner.recipe.query.listrecipes.*;
 import lgrimm.fooddesigner.root.*;
 import lgrimm.fooddesigner.source.query.listsources.*;
-import lgrimm.fooddesigner.source.query.searchsources.*;
+import lgrimm.fooddesigner.subingredient.query.listsubingredients.*;
+import lgrimm.fooddesigner.volume.query.listvolumes.*;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,32 +16,40 @@ class FoodDesignerApplicationTests {
 
 	private final ListGatewayController listGatewayController;
 	private final RootController rootController;
-//	private final ListSourcesController listSourcesController;
-//	private final SearchSourcesController searchSourcesController;
+	private final ListSourcesController listSourcesController;
+	private final ListRecipesController listRecipesController;
+	private final ListIngredientsController listIngredientsController;
+	private final ListSubIngredientsController listSubIngredientsController;
+	private final ListVolumesController listVolumesController;
 
 	@Autowired
 	FoodDesignerApplicationTests(
 			ListGatewayController listGatewayController,
-			RootController rootController
-//			ListSourcesController listSourcesController,
-//			SearchSourcesController searchSourcesController
+			RootController rootController,
+			ListSourcesController listSourcesController,
+			ListRecipesController listRecipesController,
+			ListIngredientsController listIngredientsController,
+			ListSubIngredientsController listSubIngredientsController,
+			ListVolumesController listVolumesController
 	) {
 		this.listGatewayController = listGatewayController;
 		this.rootController = rootController;
-/*
 		this.listSourcesController = listSourcesController;
-		this.searchSourcesController = searchSourcesController;
-*/
+		this.listRecipesController = listRecipesController;
+		this.listIngredientsController = listIngredientsController;
+		this.listSubIngredientsController = listSubIngredientsController;
+		this.listVolumesController = listVolumesController;
 	}
 
 	@Test
 	void contextLoads() {
 		Assertions.assertNotNull(listGatewayController);
-		Assertions.assertNotNull(rootController);
-/*
-		Assertions.assertNotNull(listSourcesController);
-		Assertions.assertNotNull(searchSourcesController);
-*/
-	}
 
+		Assertions.assertNotNull(rootController);
+		Assertions.assertNotNull(listSourcesController);
+		Assertions.assertNotNull(listRecipesController);
+		Assertions.assertNotNull(listIngredientsController);
+		Assertions.assertNotNull(listSubIngredientsController);
+		Assertions.assertNotNull(listVolumesController);
+	}
 }
